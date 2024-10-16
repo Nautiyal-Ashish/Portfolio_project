07befec3043e01258cf8d2299298e8e9037b306b
Ashun007 <157597734+Nautiyal-Ashish@users.noreply.github.com>
2024-10-16 23:56:26 +0530

    commit

---
+++ src/content_option.js
@@ -0,0 +1,138 @@
+const logotext = "PORTFOLIO";
+const meta = {
+  title: "Ashish Nautiyal",
+  description:
+    "I’m Ashish Nautiyal Student _ Full stack devloper,currently working in LearnFlow Services",
+};
+
+const introdata = {
+  title: "I’m Ashish Nautiyal",
+  animated: {
+    first: "I love coding",
+    second: "I code cool websites",
+    third: "I want to be the Best",
+  },
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
+  your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+  title: "about myself",
+  aboutme:
+    "👋 Hi, I’m Ashish Nautiyal, a passionate Full Stack Developer eager to make my mark in the tech industry. As a fresher, I’ve been honing my skills in both front-end and back-end development, with a strong foundation in React, Node.js, Express, MongoDB, and more. I’m driven by a deep love for coding and problem-solving, constantly looking to grow and learn.",
+};
+const worktimeline = [
+  {
+    jobtitle: "Web Development Intern",
+    where: "Learn Flow Services",
+    date: "Oct-2024",
+  },
+  {
+    jobtitle: "Full Stack Web Dev Student",
+    where: "Coding Blocks",
+    date: "Feb-24 Jul-24",
+  },
+];
+
+const skills = [
+  {
+    name: "MongoDB",
+    value: 78,
+  },
+  {
+    name: "Express.js",
+    value: 70,
+  },
+  {
+    name: "Javascript",
+    value: 90,
+  },
+  {
+    name: "React",
+    value: 85,
+  },
+  {
+    name: "Jquery",
+    value: 70,
+  },
+  {
+    name: "Node.js",
+    value: 75,
+  },
+];
+
+const services = [
+  {
+    title: "Web Development",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  },
+  {
+    title: "MERN Development",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "https://plus.unsplash.com/premium_photo-1683288295814-84a199da83d9?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "Full Stack E-commerce Project",
+    link: "https://ecommerce-project-ashishnautiyal.onrender.com/",
+  },
+  {
+    img: "https://plus.unsplash.com/premium_photo-1661779797085-4f5fd625d398?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "InternShip Job Portal Backend",
+    link: "https://github.com/Nautiyal-Ashish/Internship-job-portal",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1589561253898-768105ca91a8?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "Quotes App",
+    link: "https://github.com/Nautiyal-Ashish/Quotes-App-Full-Stack",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1458682625221-3a45f8a844c7?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "Color Changer",
+    link: "https://nautiyal-ashish.github.io/JS-Color-Chanager-Live-Project/",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1603791445824-0050bd436b6d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "Gmail Clone App",
+    link: "https://full-stack-clone-69961.web.app/",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1729102203128-ecea93af2609?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "Portfoilo Webiste",
+    link: "",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "nauty9625ashish@gmail.com",
+  YOUR_FONE: "+91-931831-1849",
+  description:
+    " Got questions or want to collaborate? Reach out to me, and let's connect! Im always open to new opportunities and discussions. Feel free to drop a message!",
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+  github: "https://github.com/Nautiyal-Ashish",
+  facebook: "https://facebook.com",
+  linkedin: "https://www.linkedin.com/in/ashish-nautiyal-1445292b0/",
+  twitter: "https://x.com/Nauty9625Ashish",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
